7092fcb2f3667b9e2642fd086cd0640a2544999b
Pedro Rico Pinazo <ricopinazo@gmail.com>
2025-02-28 17:48:38 +0000

    fix astrodb example

--- src/deployments/manager.rs
+++ src/deployments/manager.rs
@@ -95,7 +95,6 @@ impl Manager {
 
     #[tracing::instrument]
     pub(crate) async fn get_container_by_hostname(&self, hostname: &str) -> Option<Arc<Container>> {
-        println!("trying to access container for hostname {hostname}");
         let container = self
             .deployments
             .read()
